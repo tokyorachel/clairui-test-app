dd426315a9e7ec9097ec03797ffd8a36d79eebfb
tokyorachel <42755840+tokyorachel@users.noreply.github.com>
2022-02-19 21:11:55 +0900

    Test Clair UI locally.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import '@factorizeteam/clair-ui/dist/clairui.css';
+import { CUIAvatar, CUIButton, CUIToggle } from '@factorizeteam/clair-ui';
 
 function App() {
   return (
@@ -15,8 +17,16 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
+        <CUIButton>Use Clair UI</CUIButton>
+        <CUIButton color="abricot">Use Clair UI</CUIButton>
+        <CUIAvatar
+          src="https://randomuser.me/api/portraits/women/18.jpg" />
+        <CUIAvatar
+          square
+          src="https://randomuser.me/api/portraits/men/18.jpg" />
+        <CUIToggle id="checkbox-1" color='citron' checked={true} />
+        <CUIToggle id="checkbox-2" />
       </header>
     </div>
   );
